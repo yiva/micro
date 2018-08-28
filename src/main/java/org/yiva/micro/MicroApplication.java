f75a57e883cba91241f686ed0a56a97c763f992d
yiva <wyhtsh@gmail.com>
2018-08-28 23:26:21 +0800

    Initial commit

---
+++ src/main/java/org/yiva/micro/MicroApplication.java
@@ -0,0 +1,12 @@
+package org.yiva.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroApplication.class, args);
+    }
+}
